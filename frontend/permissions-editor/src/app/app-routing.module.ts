ba2593f4de72a3b59481ea3f884a6eb84bab6a03
SebastianDochia <sebastian.dochia@uipath.com>
2021-07-27 10:48:33 +0300

    feat: added routing

--- frontend/permissions-editor/src/app/app-routing.module.ts
+++ frontend/permissions-editor/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
+import { PermissionsComponent } from './permissions/permissions.component';
+import { PreviewComponent } from './preview/preview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'permissions', component: PermissionsComponent},
+  {path: 'preview', component: PreviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
