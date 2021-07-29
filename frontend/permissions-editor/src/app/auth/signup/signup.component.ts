86759357a191a284a67461b879c73c76053fe2af
Sebastian <sebastian.dochia@uipath.com>
2021-07-29 18:25:58 +0300

    feat: sign-up form

--- frontend/permissions-editor/src/app/auth/signup/signup.component.ts
+++ frontend/permissions-editor/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomErrorStateMatcher } from 'src/app/util/customErrorStateMatcher';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  signupForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(30)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  });
+
+  matcher = new CustomErrorStateMatcher();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    
   }
 
 }
