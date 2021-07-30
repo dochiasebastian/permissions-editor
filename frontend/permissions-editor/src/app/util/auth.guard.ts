f7b431718dd2899db7ad7c0683ae8c0ef4b3644a
Sebastian <sebastian.dochia@uipath.com>
2021-07-30 13:21:44 +0300

    fix: routing

--- frontend/permissions-editor/src/app/util/auth.guard.ts
+++ frontend/permissions-editor/src/app/util/auth.guard.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const currentUser = this.authenticationService.currentUserValue;
-        if (currentUser.name) {
+        if (currentUser.name || currentUser.token) {
             return true;
         }
 
